2fefa71e0ff3a9fd5d5903f2a6014d2836efcf37
mohamad ali <mohamadalitellawi@gmail.com>
2020-05-18 00:04:29 +0300

    Add project files.

---
+++ RegularExpressionSamples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressionSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var patterns = new List<string> { "a*b","a+b","a?b" };
+            var inputs = new List<string> {"a","b","ab","aab","abb" };
+
+            patterns.ForEach(pattern =>
+            {
+                Console.WriteLine($"Regular Expression: {pattern }");
+                var regex = new Regex(pattern);
+                inputs.ForEach(input =>
+                {
+                    Console.WriteLine($"Input Pattern: {input }");
+                    var reults = regex.Matches(input);
+
+                });
+            });
+        }
+    }
+}
